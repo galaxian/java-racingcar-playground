e42bc6ed1a3a66c45a1694b8e72f665f21096d3b
Lee <94841127+galaxian@users.noreply.github.com>
2022-08-25 21:47:25 +0900

    feat : customDeilmiter 사용 시 적용하는 기능 구현

--- src/main/java/stringAddCalculator/StringAddCalculator.java
+++ src/main/java/stringAddCalculator/StringAddCalculator.java
@@ -1,9 +1,20 @@
 package stringAddCalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringAddCalculator {
     public static int splitAndSum(String text) {
         if (isNullOrBlank(text)) {
             return 0;
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+
+            return sum(toInt(tokens));
         }
 
         return sum(toInt(split(text)));
