4eda6601242485e842cfdef6efa21d5b90b8f6ea
Lee <94841127+galaxian@users.noreply.github.com>
2022-08-25 20:55:23 +0900

    feat : ,와 :을 구분자로 하는 덧셈 기능 구현

--- src/main/java/stringAddCalculator/StringAddCalculator.java
+++ src/main/java/stringAddCalculator/StringAddCalculator.java
@@ -5,7 +5,14 @@ public class StringAddCalculator {
         if (isNullOrBlank(text)) {
             return 0;
         }
-        return 0;
+
+        String[] values = text.split("[,:]");
+        int result = 0;
+        for (String value : values) {
+            result += Integer.parseInt(value);
+        }
+
+        return result;
     }
 
     private static boolean isNullOrBlank(String text) {
